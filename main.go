9f41f179b4ff19e15f71bf3051711b17f5dbce71
Pasquale Toscano <pasquale.toscano@astrokube.com>
2021-07-06 19:42:52 +0200

    Fix duplicated webhook

--- main.go
+++ main.go
@@ -105,10 +105,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "RegistryCredentials")
 		os.Exit(1)
 	}
-	if err = (&registryv1alpha1.RegistryCredentials{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "RegistryCredentials")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
